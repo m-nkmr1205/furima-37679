4831134dc6604fee531cd1348718a00b62906bb6
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-01 22:21:27 +0900

    category_idモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+
+  # validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
