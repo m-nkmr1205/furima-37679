aef970db1f7da42b8e1c5cb7f41e156159ac8603
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-01 22:28:39 +0900

    condition_id モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
+  belongs_to :condition_id 
 
-  # validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+# validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
