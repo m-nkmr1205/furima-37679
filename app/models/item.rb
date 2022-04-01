b447ca8213b20ee0ec7921a794b6a05d0c625b7f
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-01 23:58:39 +0900

    preparation_dayモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,11 @@ class Item < ApplicationRecord
   belongs_to :condition_id 
   belongs_to :postage_type_id
   belongs_to :prefecture_id
+  belongs_to :preparation_day_id 
 
 # validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
 # validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
 # validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"}
-# # validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}  
+# validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+# validates :preparation_day, numericality: { other_than: 1 , message: "can't be blank"}  
 end
