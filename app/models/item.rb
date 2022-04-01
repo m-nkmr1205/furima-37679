75538f9781c45ea4acbaa784c6703e3f31a556c3
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-01 23:47:51 +0900

    postage_type_idモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
   belongs_to :condition_id 
+  belongs_to :postage_type_id
 
 # validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+# validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+# validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
