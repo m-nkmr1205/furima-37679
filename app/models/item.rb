f77e94539e38507254dda1349b0fa6326d98e64d
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-02 12:32:38 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,16 @@ class Item < ApplicationRecord
   belongs_to :preparation_day_id 
   has_one_attached :image
 
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id , presence: true
+  validates :postage_type_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, :greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
+
+
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"}
