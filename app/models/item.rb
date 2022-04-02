714d186a34c1c874a6c990a4832b689ab62689bc
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-02 12:04:40 +0900

    ActiveHashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category_id
   belongs_to :condition_id 
   belongs_to :postage_type_id
@@ -7,9 +8,9 @@ class Item < ApplicationRecord
   belongs_to :preparation_day_id 
   has_one_attached :image
 
-# validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-# validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-# validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"}
-# validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-# validates :preparation_day, numericality: { other_than: 1 , message: "can't be blank"}  
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :postage_type_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :preparation_day, numericality: { other_than: 1 , message: "can't be blank"}  
 end
