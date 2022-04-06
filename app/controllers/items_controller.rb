0ab0302139c49a1c0e3757a43699469642e3ac83
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-06 11:32:23 +0900

    ログイン状態による条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_item, only: [:show]
   before_action :move_to_index, except: [:index, :show]
 
