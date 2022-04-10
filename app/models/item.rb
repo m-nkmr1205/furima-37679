7909dd9c8aed0cf9d1579063a1d51cef38926841
m-nkmr1205 <lucciola0322@gmail.com>
2022-04-10 20:48:21 +0900

    sold out表示の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :preparation_day
   has_one_attached :image
+  has_one    :purchase_record
+
 
   validates :item_name, presence: true
   validates :description, presence: true
